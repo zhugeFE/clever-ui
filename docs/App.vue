de72f2a2d4603f55a12530363e0a172e615c86fe
董永强 <yqdong@37degree.com>
2019-04-02 15:47:35 +0800

    update

--- docs/App.vue
+++ docs/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <keep-alive>
-      <router-view></router-view>
+      <router-view class="top_"></router-view>
     </keep-alive>
+    <a href="http://www.miitbeian.gov.cn/state/outPortal/loginPortal.action" target="_blank">冀ICP备18028016号-1</a>
   </div>
 </template>
 
@@ -20,4 +21,13 @@ export default {
   height: 100%
   overflow: hidden
   font-family: $font-family
+  display: flex
+  flex-direction: column
+  .top_
+    flex: 1
+  a
+    flex: none
+    font-size: $size-font-tip
+    color: $color-brand
+    text-align: center
 </style>
